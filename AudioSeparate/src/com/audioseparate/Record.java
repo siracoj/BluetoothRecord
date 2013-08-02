3dcf66ef429f4527af94637b501722b037cf2cc3
siracoj <siracoj@wit.edu>
2013-08-02 03:21:02 -0400

    Added Audio Data read in

--- AudioSeparate/src/com/audioseparate/Record.java
+++ AudioSeparate/src/com/audioseparate/Record.java
@@ -4,11 +4,8 @@ import java.io.IOException;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.hardware.Camera;
 import android.media.AudioManager;
 import android.media.MediaRecorder;
